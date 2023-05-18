01ec5ba8a974c2ffde028b319d6f9e111094396e
KT <132851569+kumih0@users.noreply.github.com>
2023-05-17 19:01:02 -0500

    beginning work on javascript, generatepassword()

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+console.log(generateBtn);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +11,58 @@ function writePassword() {
 
 }
 
+console.log(writePassword);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+
+    var pwlength = prompt("Enter the desired length of your password (8-128 characters)");
+
+    if (typeof pwlength!=="number"|| pwlength.value<8 || pwlength.value>128) {
+      alert("You've entered an invalid input. Please enter a number between 8-128.");
+      return generatePassword();
+    }
+
+    var isBigLetters = confirm("Include UPPERCASE letters in your password?");
+    if (isBigLetters!==) {
+      
+    }
+   
+
+}
+
+
+
+
+// Write password to the #password input
+// function writePassword() {
+//   // alert("working")
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// function generatePassword(){
+//   return "hello"
+// }
+ 
+
+//ask for length of a password
+  //prompt
+  //save it to a variable
+//check the length provided by user is a number and in between 8 and 128
+  
+//confirm : 4 for uppercase, lowercase, numbers, special characters
+  // save it to a variable
+//check if one of the  uppercase or lowercase or num or sp char is there
+   // combine the confirmed arrays
+// create an array for uppercase letters, lowercase letters....
+//create an array variable to have a new password with length provided by user. []
+//random item from combined array and push it to new password array and do this step up to the length of user input.
